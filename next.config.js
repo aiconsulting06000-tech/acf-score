9946b200dcd82e5cfe93881cee3e3a4ba406021b
Vincent DORANGE <contact@acfscore.com>
2026-02-20 14:21:07 +0100

    fix: autoriser images Unsplash

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   // Optimisations de performance
   images: {
+    domains: ['images.unsplash.com'],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
